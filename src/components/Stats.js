38b3ae1d287b908bfa5bab638e2b1b2d56b4bd41
JohannaMJ <johanna.mannestal@gmail.com>
2021-11-25 15:23:29 +0100

    codealong

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,33 +1,39 @@
-import React from 'react'
-import { Player } from 'components/Player'
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { game } from '../reducers/game';
+import { Player } from 'components/Player';
 
 export const Stats = () => {
-  // TODO - get the 'winner' from the game reducer
-  const winner = null
-  // TODO - get the 'nextPlayer' from the game reducer
-  const nextPlayer = 'x'
+  const winner = useSelector((store) => store.game.winner);
+  const nextPlayer = useSelector((store) => store.game.player);
+
+  const dispatch = useDispatch();
 
   const handleButtonPress = () => {
-    // TODO - dispatch the restart action
-  }
+    dispatch(game.actions.restart());
+  };
 
   if (winner) {
     return (
-      <div className="stats">
+      <div className='stats'>
         <p>
           <Player value={winner} /> wins!!!!
         </p>
-        <button type="button" onClick={handleButtonPress}>Start new game</button>
+        <button type='button' onClick={handleButtonPress}>
+          Start new game
+        </button>
       </div>
-    )
+    );
   }
 
   return (
-    <div className="stats">
+    <div className='stats'>
       <p>
         Next player <Player value={nextPlayer} />
       </p>
-      <button type="button" onClick={handleButtonPress}>Restart</button>
+      <button type='button' onClick={handleButtonPress}>
+        Restart
+      </button>
     </div>
-  )
-}
+  );
+};
